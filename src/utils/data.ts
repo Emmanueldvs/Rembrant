30c2489521f26ec49b445ff895d29317c069ef0a
Emmanuel Rojas <111702728+Emmanueldvs@users.noreply.github.com>
2024-02-24 22:01:08 -0400

    nnn
    nnn

---
+++ src/utils/data.ts
@@ -0,0 +1,40 @@
+const services = [
+    {
+        title: "Maketing en redes sociales",
+        description: "Crecer en seguidores y clientes potenciales es lo de hoy, ideemos la mejor estrategia para tus redes",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Diseno Grafico",
+        description: "Hagamos de tu imagen corporativa la puerta de entrada a nuestro futuro comercial!",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Paginas web",
+        description: "Nuestra carta de presentacion en la web debe ser el reflejo de nuestra marca, a por ello!",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Blogs",
+        description: "Escribir sobre lo que hacemos es el camino perfecto para llevar clientes a nuestra web, demostremos de que estas hecho",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Tiendas virtuales",
+        description: "Vender nuestros productos son la mejor manera de crecer como productores, cautivemos los sentidos con nuestros productos",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
+        title: "Edicion de video",
+        description: "Una excelente manera de proyectar lo que queremos decir es de manera audiovisual, demostremos nuestras ideas atravez de la musica y video",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+];
+
+export { services };
